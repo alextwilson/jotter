89a141270642d04e241884e78ce72fb0f18d0291
Simon Bao <Simonbao.sb@gmail.com>
2018-02-13 15:13:04 +0000

    Note constructor creation

---
+++ src/JavaScript/note.js
@@ -0,0 +1,18 @@
+function Note(){
+    this.fullText = "";
+}
+
+Note.prototype.createNote = function(text){
+    this.fullText = text;
+    this.abbreviatedText = this._abbreviateText(text);
+}
+
+Note.prototype.returnText = function(){
+    return this.fullText;
+}
+
+Note.prototype._abbreviateText = function(text){
+    return text.substring(0,20);
+}
+
+// document.getElementById('example').textContent
