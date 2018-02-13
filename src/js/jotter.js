696b4d0e63a00f90a0168a9cd9924c562dcb2429
reena <reena.sanjay.sharma@gmail.com>
2018-02-13 22:13:12 +0000

    added package.json,set up http server

--- src/js/jotter.js
+++
@@ -1,9 +0,0 @@
-function Jotter(){
-    this.displayList = [];
-    this.note = new Note();
-}
-
-Jotter.prototype.addNote() = function(textNote ){
-    this.displayList.push(note.createNote(textNote));
-};
-
--- test/assert.js
+++ test/assert.js
@@ -1,11 +1,12 @@
 var assert = {
+
     isTrue: function(assertionToCheck) {
       if (!assertionToCheck) {
         throw new Error("Assertion failed: " + assertionToCheck + " is not truthy");
       }
     },
 
-    isEqual: function(assertionToCheck, textToCheck){
+    isEqual: function(assertionToCheck, textToCheck) {
         if(assertionToCheck === textToCheck){
             return("Equal: " + assertionToCheck + " is " + textToCheck + ".")
         } else {
@@ -14,9 +15,9 @@ var assert = {
         }
     },
 
-    isEmpty: function(list) {
-        if (list.length === 0 ) {
-            return(list + "is empty.")
+    isEmpty: function(element) {
+        if (element.length === 0 ) {
+            return("It is empty.");
         }
     },
 
