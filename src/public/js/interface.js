04e2cf81bdfdf254806a829d91edb0d3ed829de6
alextwilson <github@alextwilson.co.uk>
2018-02-14 15:02:37 +0000

    Note abbreviations displayed as a list of links

--- src/public/js/interface.js
+++ src/public/js/interface.js
@@ -1,32 +1,20 @@
 document.addEventListener("DOMContentLoaded", function(){
-    var list = new Jotter();
-  noteSubmit.addEventListener('click', function() {  
+  var list = new Jotter();
+
+  noteSubmit.addEventListener('click', function() {
     var new_note = new Note();
     new_note.createNote(document.getElementById("note").value);
-    // console.log(new_note);
     document.getElementById("note").value = "";
     list.addNote(new_note);
-    // console.log(list);
-    refreshList();
+    refreshList()
   });
 
-
   function refreshList() {
-    var notes = ""
-    list.displayList.forEach(function(element){
-        notes = notes.concat(element.abbreviatedText + "\n")
-    });
-    console.log(notes)
-    document.getElementById("test").innerHTML = notes
-
-    var notesList = document.getElementById('notesList');
-    list.displayList.forEach(function(element) {
-        var note = document.createElement('li');
-        note.appendChild(document.createTextNode(element.abbreviatedText));
-        notesList.appendChild(note);
-        console.log(element);
-      });
+    var notes = list.displayList;
+    var result = ""
+    for (var i = 0; i < notes.length; i++) {
+      result = result + " <a href='" + notes[i].abbreviatedText + "'>"+ notes[i].abbreviatedText + "</a><br>";
+    };
+    document.getElementById('all notes').innerHTML = result;
   };
-
-
 });
