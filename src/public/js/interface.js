e721c8973784112f4afa70f05afe9e4e4b0d4bf7
alextwilson <github@alextwilson.co.uk>
2018-02-15 10:13:34 +0000

    Or with links

--- src/public/js/interface.js
+++ src/public/js/interface.js
@@ -13,16 +13,17 @@ document.addEventListener("DOMContentLoaded", function(){
     var notes = list.displayList;
     var result = ""
     for (var i = 0; i < notes.length; i++) {
-      result += notes[i].abbreviatedText + "...<button id="+ i +">View note</button><br>";
+      result += "<a id='"+ i +"' href='" + notes[i].abbreviatedText + "'>"+ notes[i].abbreviatedText + "</a>...<br>";
     };
     document.getElementById('all notes').innerHTML = result;
     addEvent();
   };
 
   function addEvent() {
-    var buttons = document.getElementsByTagName("button");
-    for (i = 0; i < buttons.length; i++) {
-      buttons[i].addEventListener("click", function() {
+    var links = document.getElementsByTagName("a");
+    for (i = 0; i < links.length; i++) {
+      links[i].addEventListener("click", function() {
+        event.preventDefault();
         alert(list.displayList[this.id].fullText);
       });
     };
