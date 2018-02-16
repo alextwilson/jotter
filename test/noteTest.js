9af16ba2075cabd933381997387b135947c625c2
alextwilson <github@alextwilson.co.uk>
2018-02-16 11:05:57 +0000

    Testing rebuild
    Tests currently to not log pass or fail as console.log has been removed from the tests

--- test/noteTest.js
+++ test/noteTest.js
@@ -2,12 +2,11 @@ describe("Note",function(){
     var note = new Note();
     it("returns text for created note",function(){
         note.createNote('hello');
-        console.log(assert.isEqual(note.returnText(), 'hello'));
+        assert.isEqual(note.returnText(), 'hello');
     });
 
-    it(" does not returns text ",function(){
+    it("does not returns text ",function(){
         note.createNote('not hello');
-        console.log(assert.isNotEqual(note.returnText(), 'hello'));
+        assert.isNotEqual(note.returnText(), 'hello');
     });
 });
-
